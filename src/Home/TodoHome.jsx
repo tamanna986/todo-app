185850cf9b7bebd9ea09397fe36cfeb1fa1ead2b
tamanna986 <tamanna.986jahan@gmail.com>
2024-02-11 22:09:01 +0600

    first commit

---
+++ src/Home/TodoHome.jsx
@@ -0,0 +1,21 @@
+
+
+
+const TodoHome = () => {
+
+    return (
+        <div>
+            <div className="flex gap-1 items center justify-center">
+                <img className="w-10" src="/src/assets/logo.jpg" alt="" />
+                <h1 className="font-bold text-red-600 text-3xl">To-Do List</h1>
+            </div>
+            <div className="w-1/2 lg:w-[700px] mx-auto my-8  px-32">
+
+                <div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TodoHome;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import TodoHome from './Home/TodoHome.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <TodoHome></TodoHome>,
+  
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
